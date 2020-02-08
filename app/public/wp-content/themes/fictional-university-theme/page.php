c0705fc2eb219e7744075c77faf54541f0467a22
Danny Rhee <dongyunrhee@gmail.com>
2020-02-08 01:33:56 -0800

    Menu of Child Page links

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -14,25 +14,39 @@
         </div>
 
         <div class="container container--narrow page-section">
+            <?php 
+                $parent = wp_get_post_parent_ID(get_the_ID());
 
-        <?php 
-            $parent = wp_get_post_parent_ID(get_the_ID());
-            $test = get_the_ID();
-
-            if($parent) { ?>
-                <div class="metabox metabox--position-up metabox--with-home-link">
-                <p><a class="metabox__blog-home-link" href="<?php echo the_permalink($parent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent)?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
-                </div>
-            <?php }
-        ?>
+                if($parent) { ?>
+                    <div class="metabox metabox--position-up metabox--with-home-link">
+                        <p><a class="metabox__blog-home-link" href="<?php echo the_permalink($parent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent)?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+                    </div>
+                <?php }
+            ?>
             
-            <!-- <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-            </div> -->
+            <?php 
+                if (get_children(get_the_ID()) || $parent) { ?>
+                    <div class="page-links">
+                        <h2 class="page-links__title"><a href="<?php echo the_permalink($parent); ?>"><?php echo get_the_title($parent); ?></a></h2>
+                        <ul class="min-list">
+                            <?php 
+                                if ($parent) {
+                                    $findChildrenOf = $parent;
+                                } else {
+                                    $findChildrenOf = get_the_ID();
+                                }
+
+                                wp_list_pages(array(
+                                    "title_li" => Null,
+                                    "child_of" => $findChildrenOf,
+                                    // Sorting the column orders by the orders defined in the WP admin page
+                                    "sort_column" => "menu_order"
+                                ));
+                            ?>
+                        </ul>
+                    </div>
+                <?php }
+            ?>
 
             <div class="generic-content">
                 <?php the_content(); ?>
